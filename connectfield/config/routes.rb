1122ef6d8ee957bb555d49b1b131b7cea12a38d4
Muyideen Khalifa <dixonmk400@gmail.com>
2022-07-31 16:54:46 +0100

    change route from /users/sign_in to /login

--- connectfield/config/routes.rb
+++ connectfield/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
   # Defines the root path route ("/")
